aad3eb87fae0b72c73252430cce7e04990344cf4
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2022-07-19 14:23:58 +0300

    added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+
+
 class Square:
-    def __init__(self, size):
+    """Square class with validated private instance attribute size"""
+
+    def __init__(self, size=0):
+        """Args:
+               size: size of square
+        """
         self.__size = size
