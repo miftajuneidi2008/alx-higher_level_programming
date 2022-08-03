ed4024664ca9fec1a8bf4f6701fac4b2fe3e29de
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2022-08-03 09:08:45 +0300

    added

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""append after module"""
+'''append_after'''
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """append a text after a substring line"""
+	'''search and update'''
+	read = []
+	with open(filename, "r", encoding="utf-8") as f:
+		read = f.readlines()
+		index = 0
 
-    with open(filename, 'r') as f:
-        content = f.readlines()
-        for (index, line) in enumerate(content):
-            if line.find(search_string) != -1:
-                content.insert(index+1, new_string)
-        new_content = "".join(content)
-    f = open(filename, 'w')
-    f.write(new_content)
-    f.close()
+		while index < len(read):
+			if search_string in read[index]:
+				read[index:index + 1] = [read[index], new_string]
+				index += 1
+			index += 1
+
+	with open(filename, "w", encoding="utf-8") as file:
+		file.writelines(read)
