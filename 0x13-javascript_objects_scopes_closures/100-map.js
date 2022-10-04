52f2db50e14f0d97d2dfa826195dfffa18ee7343
mifta <92078416+miftajuneidi2008@users.noreply.github.com>
2022-10-04 16:00:05 +0300

    added

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-
 console.log(list);
-console.log(list.map((x, i) => x * i));
+const newList = list.map((item, index) => item * index);
+console.log(newList);
